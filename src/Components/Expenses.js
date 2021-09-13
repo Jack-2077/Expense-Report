c5c4c6eff3fdaae9e844d22506eaed5320f45f4e
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-09-13 08:42:42 -0700

    Add Card component for container

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.data.map((expense) => (
         <ExpenseItem
           title={expense.title}
@@ -11,7 +12,7 @@ function Expenses(props) {
           date={expense.date}
         />
       ))}
-    </div>
+    </Card>
   );
 }
 
