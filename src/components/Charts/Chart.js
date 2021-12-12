921f012b73ee3a97447d97bc1b7289ae54181caa
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-12-12 12:02:05 -0800

    Find max value from data points and pass it to the charts component

--- src/components/Charts/Chart.js
+++ src/components/Charts/Chart.js
@@ -3,13 +3,20 @@ import React from "react"
 import ChartBar from "./ChartBar"
 import "./Chart.css"
 const Chart = props => {
+
+  const dataPointsValues = props.dataPoints.map(
+    dataPoint => dataPoint.data
+  )
+
+  const maxValue = Math.max(...dataPointsValues)
+
  return (
    <div className="chart">
    {props.dataPoints.map(dataPoint => 
    <ChartBar 
    key={dataPoint.label}
    data={dataPoint.data} 
-   maxValue={null} 
+   maxValue={maxValue} 
    label={dataPoint.label}/> )}
    </div>
  )
