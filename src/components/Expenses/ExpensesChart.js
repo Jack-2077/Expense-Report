7fe0d1f273362a47ca06f240aa1fc7060b1c0ce0
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-12-12 11:53:47 -0800

    Add values to data points from props

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import Chart from "../Charts/Chart"
+const ExpensesChart = props => {
+
+  const dataPoints = [
+    {label: 'Jan', data: 0},
+    {label: 'Feb', data: 0},
+    {label: 'Mar', data: 0},
+    {label: 'Apr', data: 0},
+    {label: 'May', data: 0},
+    {label: 'Jun', data: 0},
+    {label: 'Jul', data: 0},
+    {label: 'Aug', data: 0},
+    {label: 'Sep', data: 0},
+    {label: 'Oct', data: 0},
+    {label: 'Nov', data: 0},
+    {label: 'Dec', data: 0}
+  ]
+
+  for(const expense in props.expense){
+    const expenseMonth = expense.date.getMonth()
+    dataPoints[expenseMonth].data += expense.amount
+  }
+  return <Chart dataPoints={dataPoints}/>
+}
+
+export default ExpensesChart
