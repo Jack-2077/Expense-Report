29e2c3b6aed707f2d8d99bfb7eaf74a54e925c18
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-12-12 11:28:35 -0800

    Add barFillHeight to conditionally change the bar height depending upon the max value

--- src/components/Charts/ChartBar.js
+++ src/components/Charts/ChartBar.js
@@ -3,8 +3,18 @@ import React from "react"
 import "./ChartBar.css"
 
 const ChartBar = props => {
-  return (
+  let barFillHeight = '0%'
 
+ if(props.max > 0){
+   barFillHeight = Math.random((props.value/props.max)*100)+ '%'
+ }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div> 
   )
 }
 
